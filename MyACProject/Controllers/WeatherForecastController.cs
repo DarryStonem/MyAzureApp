41de30dc80e8fc0db7bfa4b0db1b0b25f1c4a9b6
Alex González <alex@darry.dev>
2020-05-20 09:18:16 -0500

    Change to API 2

--- MyACProject/Controllers/WeatherForecastController.cs
+++ MyACProject/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace MyACProject.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Humberto", "FirstCampCloud", "Luis", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Humberto", "FirstCampCloud", "FirstCampCloud", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
